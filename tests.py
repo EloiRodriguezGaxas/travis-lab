b77f031ea676187e7abd460e532e06fa8ecf3b3f
Eloi Rodriguez Gaxas <eloirodriguezgaxas@c161109.wlan.net.ed.ac.uk>
2019-10-23 10:42:16 +0100

    added test negative

--- tests.py
+++ tests.py
@@ -7,5 +7,9 @@ class TestHelloFile(unittest.TestCase):
         result = hello.addTwoNumbers(1, 3)
         self.assertEqual(result, 4)
 
+    def test_negative(self):
+        result = hello.addTwoNumbers(-1, -10)
+        self.assertEqual(result, -11)
+
 if __name__ == '__main__':
     unittest.main()
