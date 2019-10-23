e5e62a00ffc8cd379d2ec90b06b62233a63fdd86
Eloi Rodriguez Gaxas <eloirodriguezgaxas@c161109.wlan.net.ed.ac.uk>
2019-10-23 10:39:13 +0100

    changed travis.yml to run the tests

--- tests.py
+++ tests.py
@@ -7,16 +7,5 @@ class TestHelloFile(unittest.TestCase):
         result = hello.addTwoNumbers(1, 3)
         self.assertEqual(result, 4)
 
-"""     def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2) """
-
 if __name__ == '__main__':
     unittest.main()
